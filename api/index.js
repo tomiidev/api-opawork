dbf2c1c14c494c962c20ddc19f62495511e2bff3
tomiidev <tomasruglio21@gmail.com>
2024-08-06 02:18:10 -0300

    credentials

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ config()
 
 
 const app = express()
-app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT"] }))
+app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT"], credentials:true }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
