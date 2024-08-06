53589f7196f9344cfc576d6ba1db895b3a8becfe
tomiidev <tomasruglio21@gmail.com>
2024-08-06 13:16:45 -0300

    firebase.json

--- firebase.js
+++ firebase.js
@@ -2,11 +2,13 @@ import dotenv from "dotenv"
 dotenv.config()
 import { applicationDefault, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth"
+import admin from "firebase-admin"
 initializeApp({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    serviceAccountId: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
-    
+    credential: admin.credential.cert({
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        projectId: process.env.FIREBASE_PROJECT_ID
+    })
 
 })
 
