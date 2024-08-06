af1cb12846b6153285645227c9cb51a840a4a841
tomiidev <tomasruglio21@gmail.com>
2024-08-06 13:07:01 -0300

    aaa

--- firebase.js
+++ firebase.js
@@ -3,11 +3,12 @@ dotenv.config()
 import { applicationDefault, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth"
 initializeApp({
-    serviceAccountId: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     projectId: process.env.FIREBASE_PROJECT_ID,
+    serviceAccountId: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
     
+
 })
 
-export const auth = getAuth()
+export const auth = getAuth();
 
