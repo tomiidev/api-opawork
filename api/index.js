2385fb3f6b3c1dafa1df66eeb88d28e044b70648
tomiidev <tomasruglio21@gmail.com>
2024-08-06 01:55:11 -0300

    method

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ config()
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT"] }))
 app.use(router)
 
 
