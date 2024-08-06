11520bb2076ff9283583e6a3904d5ec57870654f
tomiidev <tomasruglio21@gmail.com>
2024-08-06 16:03:34 -0300

    match

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -150,10 +150,10 @@ router.post("/api/login", cors(), async (req, res) => {
         const sessionToken = token; // Para simplicidad, estamos usando el mismo token
 
         // Configurar la cookie con el token de sesión
-       /*  res.cookie('session', sessionToken, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-        }); */
+        /*  res.cookie('session', sessionToken, {
+             httpOnly: true,
+             secure: process.env.NODE_ENV === 'production',
+         }); */
 
         return res.status(200).json({
             message: 'Login exitoso',
@@ -165,6 +165,52 @@ router.post("/api/login", cors(), async (req, res) => {
                 type: user.type
                 // Añade otros campos que quieras devolver
             }
+        });
+    } catch (error) {
+        console.error('Error verifying token:', error);
+        return res.status(401).json({ error: 'No autorizado' });
+    }
+    finally {
+        clientDB.close();
+    }
+})
+router.post("/api/match/:id", cors(), async (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.setHeader('Access-Control-Allow-Origin', `https://opawork.vercel.app`);
+    res.setHeader('Access-Control-Allow-Credentials', "true");
+    const { token } = req.body;
+
+    if (!token) {
+        return res.status(401).json({ error: 'Token inválido' });
+    }
+
+    try {
+        // Verificar el token de Google con Firebase Admin
+        const decodedToken = await auth.verifyIdToken(token);
+        console.log(decodedToken);
+        const userEmail = decodedToken.email;
+
+        // Buscar usuario en la base de datos
+        await clientDB.connect()
+        const jobs = clientDB.db("opawork").collection("job").find();
+
+        if (!jobs) {
+            return res.status(404).json({ error: 'No hay trabajos' });
+        }
+
+        // Generar un token de sesión (puedes usar el mismo token o un JWT personalizado)
+        const sessionToken = token; // Para simplicidad, estamos usando el mismo token
+
+        // Configurar la cookie con el token de sesión
+        /*  res.cookie('session', sessionToken, {
+             httpOnly: true,
+             secure: process.env.NODE_ENV === 'production',
+         }); */
+
+        return res.status(200).json({
+            message: 'Login exitoso',
+            job: jobs
         });
     } catch (error) {
         console.error('Error verifying token:', error);
