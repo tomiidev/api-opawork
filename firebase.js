8e7d5c061914640c3b6c6ee2c120908ce3eda967
tomiidev <tomasruglio21@gmail.com>
2024-08-06 12:48:03 -0300

    firebase

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ dotenv.config()
 import { applicationDefault, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth"
 initializeApp({
-    credential: applicationDefault()
+    serviceAccountId: "firebase-adminsdk-931wp@opawork-d9583.iam.gserviceaccount.com"
 })
 
 export const auth = getAuth()
