f313f27d89a3080b882d90f7e37b4ac85da0126a
tomiidev <tomasruglio21@gmail.com>
2024-08-06 14:04:17 -0300

    .env

--- firebase.js
+++ firebase.js
@@ -4,10 +4,11 @@ import { applicationDefault, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth"
 import admin from "firebase-admin"
 const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
-
+console.log(privateKey)
 initializeApp({
     credential: admin.credential.cert({
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
         privateKey: privateKey,
         projectId: process.env.FIREBASE_PROJECT_ID
     })
