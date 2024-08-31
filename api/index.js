6d39e8f12c8d328867b97fdf9d61f0c7f98f1502
tomiidev <tomasruglio21@gmail.com>
2024-08-31 20:53:49 -0300

    c

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ config()
 
 
 const app = express()
-app.use(cors({ origin: ["https://olamercado..vercel.app", "http://localhost:3030"], methods: "GET, POST, PUT, DELETE", credentials: true }))
+app.use(cors({ origin: ["https://olamercado.vercel.app", "http://localhost:3030"], methods: "GET, POST, PUT, DELETE", credentials: true }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
