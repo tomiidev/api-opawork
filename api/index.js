13f1e1fb94d9626f2befdd188119a228ebdd9dfa
tomiidev <tomasruglio21@gmail.com>
2024-08-31 23:58:36 -0300

    a

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ config()
 
 
 const app = express()
-app.use(cors({ origin: ["https://unabuena.com", "http://localhost:3030"], methods: "GET, POST, PUT, DELETE", credentials: true }))
+app.use(cors({ origin: ["https://unabuenauy.com", "http://localhost:3030"], methods: "GET, POST, PUT, DELETE", credentials: true }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
