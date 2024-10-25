b257f86b29ebfa17bbeb1c97b7719a1cf990d290
tomiidev <tomasruglio21@gmail.com>
2024-10-24 21:30:22 -0300

    object user changed

--- api/controllers/user_controller.js
+++ api/controllers/user_controller.js
@@ -91,9 +91,9 @@ export const login = async (req, res) => {
         if (!user) {
             return res.status(401).json({ message: 'Usuario no encontrado' });
         }
-        console.log(user)
+       
         // Comparar la contraseña proporcionada con la almacenada usando bcrypt
-        const isPasswordValid = await bcrypt.compare(password, user[0].password);
+        const isPasswordValid = await bcrypt.compare(password, user.password);
         if (!isPasswordValid) {
             return res.status(401).json({ message: 'Credenciales inválidas' });
         }
