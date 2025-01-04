19c31bcc5611fbd5eb02619b21a9003f4f3e78bf
tomiidev <tomasruglio21@gmail.com>
2025-01-04 20:50:13 -0300

    origin serveer

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ import cookieParser from "cookie-parser";
 config();
 
 const app = express();
-app.use(cors({ origin: ["https://keplan-admin-oficial.vercel.app","https://signin.unabuenauy.com", "http://localhost:3000", "http://localhost:5173", "http://localhost:5174"], methods: "GET, POST, PUT, DELETE, OPTIONS", credentials: true }));
+app.use(cors({ origin: ["https://admin.unabuenauy.com","https://signin.unabuenauy.com", "http://localhost:3000", "http://localhost:5173", "http://localhost:5174"], methods: "GET, POST, PUT, DELETE, OPTIONS", credentials: true }));
 /* app.options('*', cors()); */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
