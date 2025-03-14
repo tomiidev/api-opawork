2fccc4b85008674b669fc9a760b4740670ae3ddf
tomiidev <tomasruglio21@gmail.com>
2025-03-14 17:18:19 -0300

    api

--- api/index.js
+++ api/index.js
@@ -17,7 +17,10 @@ import cookieParser from "cookie-parser";
 config();
 
 const app = express();
-app.use(cors({ origin: ["https://contygo.vercel.app", "http://localhost:5173", "http://localhost:5174","http://localhost:5175", "https://auth.opawork.app"], methods: "GET, POST, PUT, DELETE, OPTIONS", credentials: true }));
+app.use(cors({
+    origin: ["http://localhost:5173",
+        "http://localhost:5174", "http://localhost:5175", "https://auth.opawork.app", "https://negocios.opawork.app"], methods: "GET, POST, PUT, DELETE, OPTIONS", credentials: true
+}));
 /* app.options('*', cors()); */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
